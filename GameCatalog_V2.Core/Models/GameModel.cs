531cd5efc24c7461100ba3612a50714a1983e8f3
Maksat Muhammetorazov <maksatmyrat.muhammetorazov@ogr.sakarya.edu.tr>
2025-01-08 21:40:59 +0500

    Add component for create and edit game with mock

--- GameCatalog_V2.Core/Models/GameModel.cs
+++ GameCatalog_V2.Core/Models/GameModel.cs
@@ -1,10 +1,31 @@
-﻿namespace GameCatalog_V2.Core.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameCatalog_V2.Core.Models;
 
 public class GameModel
 {
     public int Id { get; set; } = 1;
+    [Required]
+    [MinLength(3), MaxLength(50)]
     public string? Name { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(500)]
+    public string? Description { get; set; }
+    public bool InSale { get; set; }
+    public string? GamePlayVideo { get; set; }
     public int GenreId { get; set; }
+    [Required]
     public DateTime? ReleaseDate { get; set; }
     public string? Image { get; set; }
+    public string? ShortName
+    {
+        get
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                return null;
+            else if (Name.Length > 50)
+                return Name.Substring(0, 50) + "...";
+            else return Name;
+        }
+    }
 }
--- GameCatalog_V2.Core/Repository/IRepository.cs
+++ GameCatalog_V2.Core/Repository/IRepository.cs
@@ -6,7 +6,10 @@ namespace GameCatalog_V2.Core.Repository;
 public interface IRepository
 {
     List<GameModel> GetAllGames();
+    bool CreateNewGame(GameModel newGame);
+    GameModel? GetGameById(int id);
     List<Genre> GetAllGenres();
-    Genre GetGenreById(int id);
+    Genre? GetGenreById(int id);
     bool EditGenre(Genre editedgenre);
+
 }
